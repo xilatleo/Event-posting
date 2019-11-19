71de1a66f41820eef0ac8fb141b0ac7953de3b3f
xilatleo <trinhhoangquan234@gmail.com>
2019-11-18 20:09:19 -0600

    styled eventdetailed info, chat and sidebar

--- src/features/event/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { Segment, Item, Label } from "semantic-ui-react";
 
 const EventDetailedSidebar = () => {
-    return (
-        <div>
-            <h1>Side bar</h1>
-        </div>
-    )
-}
-export default EventDetailedSidebar
+  return (
+    <Fragment>
+      <Segment
+        textAlign="center"
+        style={{ border: "none" }}
+        attached="top"
+        secondary
+        inverted
+        color="teal"
+      >
+        2 People Going
+      </Segment>
+      <Segment attached>
+        <Item.Group divided>
+          <Item style={{ position: "relative" }}>
+            <Label
+              style={{ position: "absolute" }}
+              color="orange"
+              ribbon="right"
+            >
+              Host
+            </Label>
+            <Item.Image size="tiny" src="/assets/user.png" />
+            <Item.Content verticalAlign="middle">
+              <Item.Header as="h3">Attendee Name</Item.Header>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+    </Fragment>
+  );
+};
+export default EventDetailedSidebar;
